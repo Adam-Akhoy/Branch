aa5a216fba67fe97e70372b71e88091aa342a4b5
adem akhoy <akhoy.adem@gmail.com>
2021-02-26 17:06:46 -0600

    sepete ekle kismi yapildi

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,5 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("urunleri listele kismi yazildi");
+        System.out.println("sepete ekle kismi yapildi");
     }
 }
